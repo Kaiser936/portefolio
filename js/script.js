1582b810721b36d5dd486fb35067137522255b61
Soufiane MOHAMAD <soufiane.mohamad@gmail.com>
2023-05-02 14:04:50 +0200

    js commit

--- js/script.js
+++ js/script.js
@@ -15,3 +15,27 @@
         
     }
     toggleMenu();
+
+
+const txtAnim = document.querySelector('h1');
+new Typewriter(txtAnim, {
+    deleteSpeed: 20
+})
+    .changeDelay(20)
+    .typeString("Moi c'est Soufiane MOHAMAD")
+    .pauseFor(300)
+    .typeString("<strong>, Dev Full-Stack </strong>")
+    .pauseFor(1500)
+    .deleteChars(12)
+    .typeString("<span style='color: #27ae60'> CSS </span>")
+    .pauseFor(1500)
+    .deleteChars(5)
+    .typeString("<span style='color: midnightblue'> React </span>")
+    .pauseFor(1500)
+    .deleteChars(7)
+    .typeString("<span style='color: #ea39ff'> PHP </span>")
+    .pauseFor(1500)
+    .deleteChars(5)
+    .typeString("<span style='color: #ff6910'> JavaScript </span>")
+    .pauseFor(1500)
+    .start();
