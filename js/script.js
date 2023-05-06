41e9d3c73f390101fcc1eac2644fbc0a81dcab47
soufiane.mohamad@gmail.com <soufiane.mohamad@gmail.com>
2023-05-06 19:16:37 +0200

    Mise a jour du 06/05/2023

--- js/script.js
+++ js/script.js
@@ -1,41 +1,46 @@
-    function toggleMenu () {  
-        const navbar = document.querySelector('.navbar');
-        const burger = document.querySelector('.burger');
-        
-        burger.addEventListener('click', (e) => {    
-        navbar.classList.toggle('show-nav');
-        });    
-        // bonus
-        const navbarLinks = document.querySelectorAll('.navbar a');
-        navbarLinks.forEach(link => {
-        link.addEventListener('click', (e) => {    
-            navbar.classList.toggle('show-nav');
-        }); 
-        })
-        
-    }
-    toggleMenu();
+// Menu burger
+let toggle = document.querySelector('.toggle');
+let body = document.querySelector('body');
 
-
-const txtAnim = document.querySelector('h1');
-new Typewriter(txtAnim, {
-    deleteSpeed: 20
+toggle.addEventListener('click', function() {
+    body.classList.toggle('open');
 })
-    .changeDelay(20)
+
+// Typewriter
+const txtAnim = document.querySelector('h1');
+const typewriter = new Typewriter(txtAnim, {
+    deleteSpeed: 20,
+    delay: 20
+});
+
+typewriter
     .typeString("Moi c'est Soufiane MOHAMAD")
     .pauseFor(300)
     .typeString("<strong>, Dev Full-Stack </strong>")
-    .pauseFor(1500)
+    .pauseFor(1000)
     .deleteChars(12)
     .typeString("<span style='color: #27ae60'> CSS </span>")
-    .pauseFor(1500)
+    .pauseFor(1000)
     .deleteChars(5)
     .typeString("<span style='color: midnightblue'> React </span>")
-    .pauseFor(1500)
+    .pauseFor(1000)
     .deleteChars(7)
     .typeString("<span style='color: #ea39ff'> PHP </span>")
-    .pauseFor(1500)
+    .pauseFor(1000)
     .deleteChars(5)
     .typeString("<span style='color: #ff6910'> JavaScript </span>")
-    .pauseFor(1500)
+    .pauseFor(1000)
     .start();
+
+
+// Confetti
+function randomInRange(min, max) {
+    return Math.random() * (max - min) + min;
+}
+
+confetti({
+    angle: randomInRange(55, 125),
+    spread: randomInRange(50, 70),
+    particleCount: randomInRange(50, 100),
+    origin: { y: 0.6 },
+});
